a5af1698f34bf6cdcc6dee62b78913929a2534ea
theAstroGoth <theastrogoth@gmail.com>
2022-04-01 16:32:58 -0500

    set up outline for Flight Plan Illustrator App

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,12 +2,14 @@ import {Routes, Route } from 'react-router-dom';
 
 import TransferApp from '../TransferApp';
 import FlybyApp from '../FlybyApp';
+import ManeuversApp from '../ManeuversApp';
 
 function Main() {
   return (
     <Routes>
       <Route path='/' element={<TransferApp />} />
       <Route path='/Flyby/' element={<FlybyApp />} />
+      <Route path='/Maneuvers/' element={<ManeuversApp />} />
     </Routes>
   );
 }
---
+++ src/main/libs/surfaceTexture.ts
@@ -0,0 +1,26 @@
+// see https://github.com/empet/Texture-mapping-with-Plotly/blob/main/texture.py for outline of approach
+namespace SurfaceTexture {
+    // clustering
+    function kmeans(observations: number[][]) {
+        const n = observations.length;
+        const dims = observations[0].length;
+
+        // E-step
+
+        // M-step
+    }
+
+    function kmeansAssignment() {
+        
+    }
+
+    // image processing
+
+    // colorscale assignment
+
+    // triangulation
+
+
+}
+
+export default SurfaceTexture;
