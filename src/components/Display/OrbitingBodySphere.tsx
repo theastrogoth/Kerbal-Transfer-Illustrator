4a17bd62f054ae95f2326ada37ab49db867485e7
theAstroGoth <theastrogoth@gmail.com>
2022-06-23 00:43:59 -0500

    add vessel sprite

--- src/components/Display/OrbitingBodySphere.tsx
+++ src/components/Display/OrbitingBodySphere.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as THREE from 'three';
 import { OrbitingBody } from '../../main/objects/body';
 import { div3, hexFromColorString, TWO_PI, degToRad } from '../../main/libs/math';
@@ -8,29 +8,40 @@ import textures from '../../textureData';
 import { useLoader } from '@react-three/fiber';
 
 function OrbitingBodySphere({body, plotSize, date}: {body: OrbitingBody, plotSize: number, date: number}) {
+    const [color, setColor] = useState(hexFromColorString(body.color.toString()));
+    const [soiColor, setSoiColor] = useState(hexFromColorString(body.color.rescale(0.5).toString()));
+    
+    const [hasTexture, setHasTexture] = useState(textures.get(body.name) !== undefined);
+    const [textureURL, setTextureURL] = useState(textures.get(body.name) || textures.get("blank") as string);
+    const texture = useLoader(THREE.TextureLoader, textureURL);
+
     const pos = div3(Kepler.orbitToPositionAtDate(body.orbit, date), plotSize);
     const position = new THREE.Vector3(-pos.x, pos.z, pos.y);
-    const color = hexFromColorString(body.color.toString());
-    const soiColor = hexFromColorString(body.color.rescale(0.5).toString());
-    let textureURL = textures.get(body.name);
-    const hasTexture = textureURL !== undefined;
-    textureURL = textureURL || textures.get("blank") as string;
-    const texture = useLoader(THREE.TextureLoader, textureURL);
+
+    useEffect(() => {
+        setColor(hexFromColorString(body.color.toString()));
+        setSoiColor(hexFromColorString(body.color.rescale(0.5).toString()));
+        const newTextureURL = textures.get(body.name);
+        setHasTexture(newTextureURL !== undefined);
+        setTextureURL(newTextureURL || textures.get("blank") as string);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [body])
+
     return (
         <>
-        <mesh 
-            position={position} 
-            rotation={[0, degToRad(body.initialRotation) + TWO_PI * ((date % body.rotationPeriod) / body.rotationPeriod), 0]}
-            onClick={(e) => {e.stopPropagation(); console.log(e)}}
-        >
-            <sphereGeometry args={[body.radius / plotSize, 32, 32]} />
-            <meshLambertMaterial color={hasTexture ? 'white' : color} map={texture} />
-        </mesh>
-        <mesh position={position} onClick={(e) => {e.stopPropagation(); console.log(e)}}>
-            <sphereGeometry args={[body.soi / plotSize, 32, 32]} />
-            <meshBasicMaterial transparent={true} opacity={0.25} color={soiColor} />
-        </mesh>
-        <OrbitLine orbit={body.orbit} date={date} plotSize={plotSize} color={body.color} />
+            <mesh 
+                position={position} 
+                rotation={[0, degToRad(body.initialRotation) + TWO_PI * ((date % body.rotationPeriod) / body.rotationPeriod), 0]}
+                onClick={(e) => {e.stopPropagation(); console.log(e)}}
+            >
+                <sphereGeometry args={[body.radius / plotSize, 32, 32]} />
+                <meshLambertMaterial color={hasTexture ? 'white' : color} map={texture} />
+            </mesh>
+            <mesh position={position} onClick={(e) => {e.stopPropagation(); console.log(e)}}>
+                <sphereGeometry args={[body.soi / plotSize, 32, 32]} />
+                <meshBasicMaterial transparent={true} opacity={0.25} color={soiColor} />
+            </mesh>
+            <OrbitLine orbit={body.orbit} date={date} plotSize={plotSize} color={body.color} />
         </>
     )
   }
--- src/components/Display/TrajectoryDisplay.tsx
+++ src/components/Display/TrajectoryDisplay.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import * as THREE from 'three';
 import Orbit from '../../main/objects/orbit';
 import OrbitLine from './OrbitLine';
 
 import SolarSystem from '../../main/objects/system';
 import Kepler from '../../main/libs/kepler';
-import { div3 } from '../../main/libs/math';
+import { div3, vec3 } from '../../main/libs/math';
 
 import maneuverIcon from '../../assets/icons/maneuver.png'; 
 import soiIcon from '../../assets/icons/soi.png'; 
+import rocketIcon from '../../assets/icons/rocket.png';
 
+const textureLoader = new THREE.TextureLoader();
+const maneuverTexture = textureLoader.load(maneuverIcon);
+const soiTexture = textureLoader.load(soiIcon);
+const rocketTexture = textureLoader.load(rocketIcon);
 
-
-const maneuverTexture = new THREE.TextureLoader().load(maneuverIcon);
-const soiTexture = new THREE.TextureLoader().load(soiIcon);
-
-function TrajectoryDisplay({trajectory, system, date, plotSize, color = {r: 255, g: 255, b: 255}, icons = {maneuver: [], soi: []}}: {trajectory: Trajectory, system: SolarSystem, date: number, plotSize: number, color?: IColor, icons?: {maneuver: number[], soi: number[]}}) {
+function getOrbits(trajectory: Trajectory, system: SolarSystem, date: number, plotSize: number, color: IColor) {
     const trajectoryOrbits = trajectory.orbits.map((orbit, index) =>
         <OrbitLine 
             key={index}
@@ -27,6 +28,10 @@ function TrajectoryDisplay({trajectory, system, date, plotSize, color = {r: 255,
             maxDate={trajectory.intersectTimes[index+1]}
         />
     );
+    return trajectoryOrbits;
+}
+
+function getManeuverSprites(trajectory: Trajectory, icons: {maneuver: number[], soi: number[]}, plotSize: number) {
     const maneuverSprites = icons.maneuver.map(intersectIdx => { 
         const orbitIdx = Math.min(trajectory.orbits.length-1, intersectIdx) 
         const orbit = trajectory.orbits[orbitIdx];
@@ -36,6 +41,10 @@ function TrajectoryDisplay({trajectory, system, date, plotSize, color = {r: 255,
             <spriteMaterial map={maneuverTexture} sizeAttenuation={false}/>
         </sprite>
     });
+    return maneuverSprites;
+}
+
+function getSoiSprites(trajectory: Trajectory, icons: {maneuver: number[], soi: number[]}, plotSize: number) {
     const soiSprites = icons.soi.map(intersectIdx => {  
         const orbitIdx = Math.min(trajectory.orbits.length-1, intersectIdx) 
         const orbit = trajectory.orbits[orbitIdx];
@@ -45,12 +54,52 @@ function TrajectoryDisplay({trajectory, system, date, plotSize, color = {r: 255,
             <spriteMaterial map={soiTexture} sizeAttenuation={false}/>
         </sprite>
     });
-    // const activeOrbitIndex = trajectory.intersectTimes.slice(0,-1).findIndex((time, index) => date > time && date < trajectory.intersectTimes[index+1])
+    return soiSprites;
+}
+
+function getVesselSprite(trajectory: Trajectory, date: number, plotSize: number) {
+    const activeOrbitIndex = trajectory.intersectTimes.slice(0,-1).findIndex((time, index) => date > time && date < trajectory.intersectTimes[index+1])
+    if(activeOrbitIndex === -1) {
+        return <></>
+    }
+    const activePosition = activeOrbitIndex === -1 ? vec3(0,0,0) : div3(Kepler.orbitToPositionAtDate(trajectory.orbits[activeOrbitIndex], date), plotSize);
+    const vesselSprite = <sprite scale={[0.05,0.05,0.05]} position={new THREE.Vector3(-activePosition.x, activePosition.z, activePosition.y)}>
+        <spriteMaterial map={rocketTexture} sizeAttenuation={false}/>
+    </sprite>
+    return vesselSprite;
+}
+
+function TrajectoryDisplay({trajectory, system, date, plotSize, color = {r: 255, g: 255, b: 255}, icons = {maneuver: [], soi: []}}: {trajectory: Trajectory, system: SolarSystem, date: number, plotSize: number, color?: IColor, icons?: {maneuver: number[], soi: number[]}}) {
+    const [trajectoryOrbits, setTrajectoryOrbits] = useState(getOrbits(trajectory, system, date, plotSize, color));
+    const [maneuverSprites, setManeuverSprites] = useState(getManeuverSprites(trajectory, icons, plotSize));
+    const [soiSprites, setSoiSprites] = useState(getSoiSprites(trajectory, icons, plotSize));
+    const [vesselSprite, setVesselSprite] = useState(getVesselSprite(trajectory, date, plotSize));
+
+    const dateRef = useRef(date);
+
+    useEffect(() => {
+        setTrajectoryOrbits(getOrbits(trajectory, system, date, plotSize, color));
+        setManeuverSprites(getManeuverSprites(trajectory, icons, plotSize));
+        setSoiSprites(getSoiSprites(trajectory, icons, plotSize));
+        setVesselSprite(getVesselSprite(trajectory, date, plotSize));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [trajectory, system, plotSize])
+
+    useEffect(() => {
+        if(dateRef.current !== date){
+            dateRef.current = date;
+            setTrajectoryOrbits(getOrbits(trajectory, system, date, plotSize, color));
+            setVesselSprite(getVesselSprite(trajectory, date, plotSize));
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [date])
+
     return (
         <>
             {trajectoryOrbits}
             {maneuverSprites}
             {soiSprites}
+            {vesselSprite}
         </>
     );
 }
