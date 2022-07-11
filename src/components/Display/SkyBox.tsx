2f473ee7f2fe09d7d063763fa29c4d3bab36d261
theAstroGoth <theastrogoth@gmail.com>
2022-07-11 10:29:52 -0500

    fix skybox urls

--- src/components/Display/SkyBox.tsx
+++ src/components/Display/SkyBox.tsx
@@ -4,12 +4,12 @@ import { useCubeTexture } from "@react-three/drei";
 function SkyBox() {
   const { scene } = useThree();
   const texture = useCubeTexture([
-    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/main/public/assets/skybox/PositiveX.png", 
-    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/main/public/assets/skybox/NegativeX.png",
-    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/main/public/assets/skybox/PositiveY.png",
-    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/main/public/assets/skybox/NegativeY.png",
-    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/main/public/assets/skybox/PositiveZ.png",
-    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/main/public/assets/skybox/NegativeZ.png",
+    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/assets/skybox/PositiveX.png", 
+    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/assets/skybox/NegativeX.png",
+    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/assets/skybox/PositiveY.png",
+    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/assets/skybox/NegativeY.png",
+    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/assets/skybox/PositiveZ.png",
+    "https://raw.githubusercontent.com/theastrogoth/Kerbal-Transfer-Illustrator/assets/skybox/NegativeZ.png",
   ], { path: ""})
   scene.background = texture;
   return null;
